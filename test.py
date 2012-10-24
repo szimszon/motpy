78aac5d855a7ca1406381551fcbf7d1a5e49f6b8
szimszon <szimszon@szimszon-Aspire-5742G>
2012-10-24 21:41:36 +0200

    pep8

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
-# -*- coding: utf-8 -*- 
+# -*- coding: utf-8 -*-
 
-class userInfo( object ):
-	def __init__( self, config, log, username ):
-		self.config = config
-		self.log = log
-		self.username = username
-	def getInfo( self ):
-		return dict( pin = '1234',
-							secret = '1234567890123456',
-							offset = 0 )
-	def setLastOtp( self, otp ):
-		return True
+
+class userInfo(object):
+    def __init__(self, config, log, username):
+        self.config = config
+        self.log = log
+        self.username = username
+
+    def getInfo(self):
+        return dict(pin='1234',
+                            secret='1234567890123456',
+                            offset=0)
+
+    def setLastOtp(self, otp):
+        return True
